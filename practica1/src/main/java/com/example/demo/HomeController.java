33d2ca5784a9582966b74746390b4fb58d9c6e86
JorgeRodriguezUJA <149387744+JorgeRodriguezUJA@users.noreply.github.com>
2024-02-12 20:12:48 +0100

    Commit 12/02

--- practica1/src/main/java/com/example/demo/HomeController.java
+++ practica1/src/main/java/com/example/demo/HomeController.java
@@ -2,13 +2,18 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
 	
-	@RequestMapping(value="/vistausuario", method=RequestMethod.GET)
+	@GetMapping(value="/vistausuario")
 	//@GetMapping(value="vistausuario")	// De esta forma se da por hecho que es un GET
 	public String metodo(Model model) {
 		String cadenadetexto="Esto es un mensaje del controlador. Prueba segundo commit";
@@ -16,8 +21,19 @@ public class HomeController {
 		return "vista";
 	}
 	
-	@RequestMapping(value="/formulario", method=RequestMethod.GET)
-	public String formulario(Model model) {
+	//@PostMapping(value="/datosusuario")
+	//public String metodo(HttpServletRequest req) {
+	//	String usuario = req.getParameter("usuario");
+	//	String email = req.getParameter("email");
+	//	String nombre = req.getParameter("nombre");
+	//	String apellidos = req.getParameter("apellidos");
+	//	System.out.println(usuario + ", " + email + ", " + nombre + ", " + apellidos);
+	//	return "formulario";
+	//}
+	
+	@PostMapping(value="/datosusuario")
+	public String metodo(@RequestParam("usuario") String usuario, @RequestParam("email") String email, @RequestParam("nombre") String nombre, @RequestParam("apellidos") String apellidos, Model mod) {
+		System.out.println(usuario + ", " + email + ", " + nombre + ", " + apellidos);
 		return "formulario";
 	}
 	
